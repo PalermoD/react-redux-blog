ccd1ccff7ede49c22b145a6af558f8881bb08412
PalermoD <palermo.deschamps@gmail.com>
2016-06-28 19:23:29 -0400

    reducers actions state

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,11 @@
-import {FETCH_POSTS} from '../action/index';
+import {FETCH_POSTS} from '../actions/index';
+
 const INITIAL_STATE = { all: [], posts: null };
 
 export default function(state = INITIAL_STATE, action) {
 	switch(action.type) {
 		case FETCH_POSTS:
-
+           return { ...state, all: action.payload.data };
 		default:
 		  return state;
 	}
