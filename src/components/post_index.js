cdd2be4f690769d2cda661d9e31ffe4b1d3914c1
PalermoD <palermo.deschamps@gmail.com>
2016-06-29 12:48:41 -0400

    post index action dispatcher

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-	return <div>List of Posts</div>;
-};
+class PostIndex extends Component {
+	componentWillMount() {
+		this.props.fetchPosts();
+		console.log('call actiion creator to fecth data');
+	}
+
+	render() {
+		return (
+               <div>List Of Posts</div>
+			);
+	}
+}
+
+
+// function mapDispatchToProps(dispatch) {
+// 	return bindActionCreators({ fetchPosts }, dispatch );
+// }
+
+export default connect(null, { fetchPosts })(PostIndex);
